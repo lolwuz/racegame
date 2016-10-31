e355c34228930f25bc0b5ca16caba3aa093aad97
lolwuz <martenhoekstra2@gmail.com>
2016-10-31 21:28:08 +0100

    Afbeelding opgedeeld in 4 stukken
    Gnagnagna. nu doet ie het wel.

--- RaceGame/RaceGame2/Map.cs
+++ RaceGame/RaceGame2/Map.cs
@@ -13,12 +13,30 @@ namespace RaceGame2
     public class Map 
     {
         public Bitmap map1;
+
+        public Bitmap m000;
+        public Bitmap m001;
+        public Bitmap m002;
+        public Bitmap m003;
+        public Bitmap m010;
+        public Bitmap m011;
+        public Bitmap m012;
+        public Bitmap m013;
+        public Bitmap m020;
+        public Bitmap m021;
+        public Bitmap m022;
+        public Bitmap m023;
+        public Bitmap m030;
+        public Bitmap m031;
+        public Bitmap m032;
+        public Bitmap m033;
+
         public Bitmap map2;
         public List<PickUp> pickUpList = new List<PickUp>();
 
         public Map()
         {
-            int caseSwitch = 1;
+            int caseSwitch = 2;
 
             // Baan keuze + pickUp's voor de baan + (extra's?)
             switch (caseSwitch)
@@ -29,7 +47,26 @@ namespace RaceGame2
                     pickUpList.Add(new PickUp(10, 20));
                     break;
                 case 2:
-                    map1 = new Bitmap(Properties.Resources.Baan2);
+                    m000 = new Bitmap(Properties.Resources._200);
+                    m001 = new Bitmap(Properties.Resources._201);
+                    m002 = new Bitmap(Properties.Resources._202);
+                    m003 = new Bitmap(Properties.Resources._203);
+
+                    m010 = new Bitmap(Properties.Resources._210);
+                    m011 = new Bitmap(Properties.Resources._211);
+                    m012 = new Bitmap(Properties.Resources._212);
+                    m013 = new Bitmap(Properties.Resources._213);
+
+                    m020 = new Bitmap(Properties.Resources._220);
+                    m021 = new Bitmap(Properties.Resources._221);
+                    m022 = new Bitmap(Properties.Resources._222);
+                    m023 = new Bitmap(Properties.Resources._223);
+
+                    m030 = new Bitmap(Properties.Resources._230);
+                    m031 = new Bitmap(Properties.Resources._231);
+                    m032 = new Bitmap(Properties.Resources._232);
+                    m033 = new Bitmap(Properties.Resources._233);
+
                     map2 = new Bitmap(Properties.Resources.Baan2colormap);
                     pickUpList.Add(new PickUp(684, 1000));
                     pickUpList.Add(new PickUp(755, 1000));
@@ -51,8 +88,27 @@ namespace RaceGame2
         public void Draw(Graphics g, Player p, double width, double height)
         {
             
-            g.DrawImage(map1,Convert.ToSingle(-p.posX + width), Convert.ToSingle(-p.posY + height));
-            foreach(PickUp pick in pickUpList)
+            g.DrawImage(m000,Convert.ToSingle(-p.posX + width), Convert.ToSingle(-p.posY + height));
+            g.DrawImage(m001, Convert.ToSingle(-p.posX + width + 749), Convert.ToSingle(-p.posY + height));
+            g.DrawImage(m002, Convert.ToSingle(-p.posX + width + 1498), Convert.ToSingle(-p.posY + height));
+            g.DrawImage(m003, Convert.ToSingle(-p.posX + width + 2247), Convert.ToSingle(-p.posY + height));
+
+            g.DrawImage(m010, Convert.ToSingle(-p.posX + width), Convert.ToSingle(-p.posY + height + 499 ));
+            g.DrawImage(m011, Convert.ToSingle(-p.posX + width + 749), Convert.ToSingle(-p.posY + height + 499 ));
+            g.DrawImage(m012, Convert.ToSingle(-p.posX + width + 1498), Convert.ToSingle(-p.posY + height + 499));
+            g.DrawImage(m013, Convert.ToSingle(-p.posX + width + 2247), Convert.ToSingle(-p.posY + height + 499));
+
+            g.DrawImage(m020, Convert.ToSingle(-p.posX + width), Convert.ToSingle(-p.posY + height + 998));
+            g.DrawImage(m021, Convert.ToSingle(-p.posX + width + 749), Convert.ToSingle(-p.posY + height + 998));
+            g.DrawImage(m022, Convert.ToSingle(-p.posX + width + 1498), Convert.ToSingle(-p.posY + height + 998));
+            g.DrawImage(m023, Convert.ToSingle(-p.posX + width + 2247), Convert.ToSingle(-p.posY + height + 998));
+
+            g.DrawImage(m030, Convert.ToSingle(-p.posX + width), Convert.ToSingle(-p.posY + height + 1497));
+            g.DrawImage(m031, Convert.ToSingle(-p.posX + width + 749), Convert.ToSingle(-p.posY + height + 1497));
+            g.DrawImage(m032, Convert.ToSingle(-p.posX + width + 1498), Convert.ToSingle(-p.posY + height + 1497));
+            g.DrawImage(m033, Convert.ToSingle(-p.posX + width + 2247), Convert.ToSingle(-p.posY + height + 1497));
+
+            foreach (PickUp pick in pickUpList)
             {
                 pick.Draw(g, p, width, height);
             }
--- RaceGame/RaceGame2/Properties/Resources.Designer.cs
+++ RaceGame/RaceGame2/Properties/Resources.Designer.cs
@@ -63,6 +63,166 @@ namespace RaceGame2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _200 {
+            get {
+                object obj = ResourceManager.GetObject("_200", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _201 {
+            get {
+                object obj = ResourceManager.GetObject("_201", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _202 {
+            get {
+                object obj = ResourceManager.GetObject("_202", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _203 {
+            get {
+                object obj = ResourceManager.GetObject("_203", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _210 {
+            get {
+                object obj = ResourceManager.GetObject("_210", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _211 {
+            get {
+                object obj = ResourceManager.GetObject("_211", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _212 {
+            get {
+                object obj = ResourceManager.GetObject("_212", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _213 {
+            get {
+                object obj = ResourceManager.GetObject("_213", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _220 {
+            get {
+                object obj = ResourceManager.GetObject("_220", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _221 {
+            get {
+                object obj = ResourceManager.GetObject("_221", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _222 {
+            get {
+                object obj = ResourceManager.GetObject("_222", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _223 {
+            get {
+                object obj = ResourceManager.GetObject("_223", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _230 {
+            get {
+                object obj = ResourceManager.GetObject("_230", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _231 {
+            get {
+                object obj = ResourceManager.GetObject("_231", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _232 {
+            get {
+                object obj = ResourceManager.GetObject("_232", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _233 {
+            get {
+                object obj = ResourceManager.GetObject("_233", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap baan1_thumb {
             get {
                 object obj = ResourceManager.GetObject("baan1_thumb", resourceCulture);
